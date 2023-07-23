4f5eb4f12ce3760f73d64ca5072f44bc532a25ca
htavakkoli1368 <32728223+htavakkoli1368@users.noreply.github.com>
2023-07-23 23:35:22 +0430

    change movies controller

--- WebAppMVC/Controllers/MoviesController.cs
+++ WebAppMVC/Controllers/MoviesController.cs
@@ -9,10 +9,23 @@ namespace WebAppMVC.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Random()
+        public ActionResult Index (int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content($"pageindex is ={pageIndex} and sort by {sortBy}");
+        }
+        public ActionResult Random()
         {
             var movie = new Movies() {Name="Power of Learning .net core MVC" };
-            return View(movie);
+            return RedirectToAction("Index", "Home", new {page=1,sortBy="name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"id ={id} this is power of learning");
         }
     }
 }
