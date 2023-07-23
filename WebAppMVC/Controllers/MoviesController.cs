7011c7574f435bc1675f0dd87aecf9bb0b4721ec
htavakkoli1368 <32728223+htavakkoli1368@users.noreply.github.com>
2023-07-23 21:21:27 +0430

    Add project files.

---
+++ WebAppMVC/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movies() {Name="Power of Learning .net core MVC" };
+            return View(movie);
+        }
+    }
+}
